992fc66f6952a4b128687e74950497e544c41713
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-11-16 08:31:22 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import WelcomePage from "./components/WelcomePage";
+function App() {
+  return (
+    <>
+      <div>
+        <p>Bank App</p>
+        <WelcomePage />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/WelcomePage.jsx
@@ -0,0 +1,12 @@
+const WelcomePage = () => {
+  return (
+    <div>
+      <h1>WelcomePage</h1>
+      <div>
+        <h3>login</h3>
+      </div>
+    </div>
+  );
+};
+
+export default WelcomePage;
