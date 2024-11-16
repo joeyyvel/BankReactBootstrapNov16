edbf8de4adac95b33d22623548b7a44e141e39d9
joeyboi <66668752+joeyyvel@users.noreply.github.com>
2024-11-16 09:12:29 +0800

    intall bootstrap

--- src/components/WelcomePage.jsx
+++ src/components/WelcomePage.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 const WelcomePage = () => {
+  const [task, setTask] = useState();
+
   return (
-    <div>
-      <h1>WelcomePage</h1>
+    <div className="welcome__Page m-3">
+      <h1 className="text-bg-primary p-3">WelcomePage</h1>
       <div>
-        <h3>login</h3>
+        <h3>
+          Login to{" "}
+          <span style={{ fontSize: "30px", fontFamily: "Nunito" }}>
+            {" "}
+            SampleBankCom{" "}
+          </span>{" "}
+        </h3>
+        <div className="text-bg-light p-3">
+          <h6>Enter User ID</h6>
+          <h6>Enter Password</h6>
+        </div>
       </div>
     </div>
   );
